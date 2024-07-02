6b875a6930ae8120d0f837dcaa5a4c2b6c46682c
nommu moose <moose@nommumail.co.uk>
2024-07-02 12:45:39 +0200

    Minor tidying.

--- src/portobello/netstat/main.py
+++ src/portobello/netstat/main.py
@@ -15,7 +15,7 @@ def check_port(host, port):
 
 def main(cli_strings: list, portobello_config: dict):
     saved_hostnames = portobello_config['netstat']['hostnames']
-    hostname = ask_for_input_or_list_choice(saved_hostnames, 'hostname', 'hostnames', cli_strings=cli_strings)
+    hostname, return_clargs = ask_for_input_or_list_choice(saved_hostnames, 'hostname', 'hostnames', cli_strings=cli_strings)
 
     if len(cli_strings) >= 2:
         port_number = int(cli_strings[1])
@@ -23,4 +23,4 @@ def main(cli_strings: list, portobello_config: dict):
         port_number = int(input('Please enter a port number to check:\n'))
 
     print(f"####\nThe port you checked is{[' not', ''][check_port(hostname, port_number)]} open.\n####\n")
-    return [hostname, str(port_number)]
+    return return_clargs + [str(port_number)]
